8d10493bd6ddbecdc1ed4404ba38091a04d36c76
mazhiyuan <mazy@hua-cloud.com.cn>
2019-07-17 10:29:10 +0800

    首页改版&检务保障开发

---
+++ src/pages/inspection-guarantee/constant/index.js
@@ -0,0 +1,20 @@
+// 收入
+export const incomeChartConfig = [{
+	id: 'czbk',
+	name: '财政拨款',
+}, {
+	id: 'sjbz',
+	name: '上级补助',
+}, {
+	id: 'sysr',
+	name: '事业收入',
+},{
+	id: 'jysr',
+	name: '经营收入',
+},{
+	id: 'fsdwsj',
+	name: '附属单位上缴',
+},{
+	id: 'qtsr',
+	name: '其他收入',
+}];
---
+++ src/pages/inspection-guarantee/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="outer-home-page">
+        <date-picker
+                :dateChange="setSelectTime"
+        ></date-picker>
+        <left-box></left-box>
+    </div>
+</template>
+<script>
+	import { mapActions } from 'vuex';
+	import DatePicker     from '@/components/common/date-picker';
+    import LeftBox        from './components/left';
+
+	export default {
+		methods: {
+            ...mapActions('inspectionGuarantee', ['setSelectTime']),
+        },
+		components: {
+			DatePicker,
+			LeftBox
+		},
+	}
+</script>
+<style lang="scss" scoped>
+    .outer-home-page{
+        position: relative;
+        width: 100%;
+    }
+</style>
