dea7a99213d17f1046d5552f3c06ead774f24160
chenmenglin <962800574@qq.com>
2019-07-17 18:10:19 +0800

    检务保障左半边样式实现

--- src/pages/inspection-guarantee/components/left.vue
+++ src/pages/inspection-guarantee/components/left.vue
@@ -14,7 +14,44 @@
                 <span class="chart-label-dot"></span>
                 <i>支出</i>
             </div>
-            <div class="pay-content" ref="payChart"></div>
+            <div class="pay-content">
+				<div class="payBox">
+					<div class="boxborder">
+						<div class='payBox-top'>
+							<p>123,234,23</p>
+							<p>同比<i class="el-icon-top"></i>234,23<br><span>23%</span></p>
+						</div>
+					</div>
+					<div class='payBox-bottom'>总支出</div>
+				</div>
+				<div class="payBox">
+					<div class="boxborder">
+						<div class='payBox-top'>
+							<p>123,234,23</p>
+							<p>同比<i class="el-icon-top"></i>234,23<br><span>23%</span></p>
+						</div>
+					</div>
+					<div class='payBox-bottom'>功能分类支出</div>
+				</div>
+				<div class="payBox">
+					<div class="boxborder">
+						<div class='payBox-top'>
+							<p>123,234,23</p>
+							<p>同比<i class="el-icon-top"></i>234,23<br><span>23%</span></p>
+						</div>
+					</div>
+					<div class='payBox-bottom'>支出性质</div>
+				</div>
+				<div class="payBox">
+					<div class="boxborder">
+						<div class='payBox-top'>
+							<p>123,234,23</p>
+							<p>同比<i class="el-icon-top"></i>234,23<br><span>23%</span></p>
+						</div>
+					</div>
+					<div class='payBox-bottom'>支出经济分类</div>
+				</div>
+			</div>
         </div>
 		<div class="caizheng-box">
             <div class="chart-box-title">
@@ -45,7 +82,7 @@
 	import { mapGetters }                        from 'vuex';
 	import * as services                         from '../service/index';
 	import { verifyTriggerState, numberInteger } from '@/utlis/helper';
-	import { incomeChartConfig }                 from '../constant/index';
+	import { incomeChartConfig,caizhengChartConfig }                 from '../constant/index';
 
 	export default {
 		data() {
@@ -62,7 +99,9 @@
 			const params         = { ...this.getSelectDateSection, ...this.getMapCode };
 			this.oldTriggerState = params;
 			this.incomeChart     = ECharts.init(this.$refs.incomeChart);
+			this.caizhengChart   = ECharts.init(this.$refs.caizhengChart)
 			this.loadIncomeChart(params);
+			this.loadCaizhengChart();
 		},
 		updated() {
 			const params = { ...this.getSelectDateSection, ...this.getMapCode };
@@ -89,16 +128,19 @@
 					},
 					legend : {
 						orient  : 'vertical',
-						right   : 10,
-						top     : 20,
+						right   : 50,
+						top     : 50,
 						bottom  : 20,
-						data    : xAxisData,
+						textStyle : {
+							color : 'white'
+						},
+						icon:"circle",
 					},
 					series : {
 						name     : '收入',
 						type     : 'pie',
-						radius   : '55%',
-						center   : ['40%', '50%'],
+						radius   : '65%',
+						center   : ['25%', '50%'],
 						data     : seriesData,
 						label: {
             				show:false
@@ -124,6 +166,71 @@
 					  });
 				return { xAxisData, seriesData };
 			},
+			convertChartConfigcz(configList = []) {
+				const xAxisData  = [],
+					  seriesData = [];
+					  configList.map((config) => {
+						  xAxisData.push(config.id);
+						  seriesData.push(config.name)
+					  });
+				return { xAxisData, seriesData };
+			},
+			loadCaizhengChart() {
+	  			const { xAxisData, seriesData } = this.convertChartConfigcz(caizhengChartConfig);
+				this.caizhengChart.setOption({
+					color: ['#3398DB'],
+					tooltip : {
+						trigger: 'axis',
+						axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+							type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+						}
+					},
+					grid: {
+						left: '3%',
+						right: '4%',
+						bottom: '3%',
+						containLabel: true
+					},
+					xAxis : [
+						{
+							type : 'category',
+							data : xAxisData,
+							axisTick: {
+								show:false
+							},
+							axisLine:{
+								lineStyle: {
+									color: "#fff",
+								}
+							}
+						}
+					],
+					yAxis : [
+						{
+							type : 'value',
+							axisLine:{
+								show:false,
+								lineStyle: {
+									color: "#fff",
+								}
+							}
+						}
+					],
+					series : [
+						{
+							name:'直接访问',
+							type:'bar',
+							barWidth: '60%',
+							data:seriesData,
+							itemStyle:{
+								normal:{
+									color:'#5C89FF'
+								}
+							}
+						}
+					]
+				})
+			},
 		},
 		components: {}
 	};
@@ -145,15 +252,58 @@
 		.pay-box{
 			width:806px;
 			.pay-content {
-                width: 80%;
+                width: 90%;
                 height: 261px;
                 margin: 0 auto;
+				display: flex;
+        		flex-wrap: wrap;
+				.payBox{
+					width:160px;
+					margin-top:29px;
+					margin-right:24px;
+					.boxborder{
+						width: 160px; 
+						height: 160px; 
+						border-radius:50%; 
+						box-sizing: border-box; 
+    					padding: 14px;
+						background-image: -webkit-linear-gradient(top,#01AFFF,#005FE8);
+					}
+					.payBox-top{
+						width:100%;
+						height:100%;
+						text-align:center;
+						border-radius:50%;
+						padding-top:30px;
+						background:#04092A;
+						p{
+							color:#00FFF8;
+							padding-bottom:14px;
+							span{
+								color:#FF6C40;
+								padding-left:40px;
+							}
+						}
+						p:first-child{
+							font-size:18px;
+						}
+					}
+					.payBox-bottom{
+						width:160px;
+						color:#fff;
+						text-align:center;
+						margin-top:13px;
+					}
+				}
+				.payBox:last-child{
+					margin-right:0px;
+				}
             }
 		}
 		.caizheng-box{
 			width:1234px;
 			.caizheng-content {
-                width: 80%;
+                width: 100%;
                 height: 247px;
                 margin: 0 auto;
             }
--- src/pages/inspection-guarantee/constant/index.js
+++ src/pages/inspection-guarantee/constant/index.js
@@ -1,5 +1,5 @@
 // 收入
-export const incomeChartConfig = [{
+const incomeChartConfig = [{
 	id: 'czbk',
 	name: '财政拨款',
 }, {
@@ -18,3 +18,73 @@ export const incomeChartConfig = [{
 	id: 'qtsr',
 	name: '其他收入',
 }];
+//财政拨款收入分布
+const caizhengChartConfig=[{
+	id: '北京',
+	name: "2600"
+  },
+  {
+	id: '天津',
+	name: "2300"
+  },
+  {
+	id: '河北',
+	name: "7600"
+  },
+  {
+	id: '山西',
+	name: "3600"
+  },
+  {
+	id: '内蒙古',
+	name: "6600"
+  },
+  {
+	id: '山东',
+	name: "7400"
+  },
+  {
+	id: '河南',
+	name: "2600"
+  },{
+	id: '北京',
+	name: "2600"
+  },
+  {
+	id: '天津',
+	name: "2300"
+  },
+  {
+	id: '河北',
+	name: "7600"
+  },
+  {
+	id: '山西',
+	name: "3600"
+  },
+  {
+	id: '内蒙古',
+	name: "6600"
+  },
+  {
+	id: '山东',
+	name: "7400"
+  },
+  {
+	id: '河南',
+	name: "2600"
+  },{
+	id: '山西',
+	name: "3600"
+  },
+  {
+	id: '内蒙古',
+	name: "6600"
+  },
+  {
+	id: '山东',
+	name: "7400"
+  }]
+
+
+export {incomeChartConfig,caizhengChartConfig}
