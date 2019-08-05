c70863433fb254b90c9baaaf157b4de464f0c292
chenmenglin <962800574@qq.com>
2019-08-05 18:36:22 +0800

    队伍管理数据修改

--- src/pages/team-management/index.vue
+++ src/pages/team-management/index.vue
@@ -23,7 +23,7 @@
 	import Left                             from './components/left.vue';
 	import CenterBox                        from './components/Center.vue';
 	import Right                            from './components/Right.vue';
-	import { getTeamManagement }            from './service/index';
+	import { getTeamManagement,getIncumbency,getWorkingLife,getEducationSituation,getPersonnelStatusQuoList,getPersonnelEducation,getAgeDistribution }            from './service/index';
 	import { fillZero, verifyTriggerState } from '@/utlis/helper';
 
 	export default {
@@ -51,46 +51,138 @@
 		mounted() {
 		},
 		methods   : {
-			async requestTeamData() {
-				let res = await getTeamManagement({
+			// async requestTeamData() {
+			// 	let res = await getTeamManagement({
+			// 		pindex: this.pindex,
+			// 		lev   : this.lev
+			// 	});
+			// 	if(res.code === 200) {
+			// 		const data                  = res.data;
+			// 		const qgzzrs                = fillZero(data.incumbency.qgzzrs, 5).split(''),
+			// 			  { leftBox, rightBox } = this.$refs;
+			// 		this.incumbency             = {
+			// 			...data.incumbency,
+			// 			qgzzrs,
+			// 		};
+			// 		//工作年限统计
+			// 		data.workingLife.maxValue = Math.max(...Object.values(data.workingLife));
+			// 		this.workingLife          = data.workingLife;
+			// 		leftBox.loadEducationChart(data.educationSituation);
+			// 		//个检查机关人员现状
+			// 		this.personnelStatusQuos = data.personnelStatusQuos;
+			// 		leftBox.loadpersonnelStatusQuosChart(data.personnelStatusQuos);
+			// 		//最上方的
+			// 		this.personnelCategory = {
+			// 			jcgzl : fillZero(data.personnelCategory.jcgzl, 4).split(''),
+			// 			sfxzry: fillZero(data.personnelCategory.sfxzry, 4).split(''),
+			// 			yenjcg: fillZero(data.personnelCategory.yenjcg, 4).split('')
+			// 		};
+			// 		//人员学历结构
+			// 		this.personnelEducation = data.personnelEducation;
+			// 		rightBox.loadEduBgStrChart(data.personnelEducation);
+			// 		//年龄分布
+			// 		this.ageDistribution = data.ageDistribution;
+			// 		rightBox.loadAgeDistributeChart(data.ageDistribution);
+			// 	} else {
+			// 		this.$message.error(res.msg);
+			// 	}
+			// },
+			async getIncumbency(){//全国在职人数
+				let res = await getIncumbency({
 					pindex: this.pindex,
 					lev   : this.lev
 				});
 				if(res.code === 200) {
 					const data                  = res.data;
-					const qgzzrs                = fillZero(data.incumbency.qgzzrs, 5).split(''),
-						  { leftBox, rightBox } = this.$refs;
+					const qgzzrs                = fillZero(data.qgzzrs, 5).split('');
 					this.incumbency             = {
-						...data.incumbency,
+						...data,
 						qgzzrs,
 					};
-					data.workingLife.maxValue = Math.max(...Object.values(data.workingLife));
-					this.workingLife          = data.workingLife;
-					leftBox.loadEducationChart(data.educationSituation);
-
-					this.personnelStatusQuos = data.personnelStatusQuos;
-					leftBox.loadpersonnelStatusQuosChart(data.personnelStatusQuos);
-
-					this.personnelCategory = {
-						jcgzl : fillZero(data.personnelCategory.jcgzl, 4).split(''),
-						sfxzry: fillZero(data.personnelCategory.sfxzry, 4).split(''),
-						yenjcg: fillZero(data.personnelCategory.yenjcg, 4).split('')
-					};
-
-					this.personnelEducation = data.personnelEducation;
-					rightBox.loadEduBgStrChart(data.personnelEducation);
-
-					this.ageDistribution = data.ageDistribution;
-					rightBox.loadAgeDistributeChart(data.ageDistribution);
-				} else {
+				}else {
+					this.$message.error(res.msg);
+				}
+			},
+			async getEducationSituation(){//教育情况
+				let res = await getEducationSituation({
+					pindex: this.pindex,
+					lev   : this.lev
+				});
+				if(res.code === 200) {
+					const data                  = res.data;
+					const { leftBox, rightBox } = this.$refs;
+					leftBox.loadEducationChart(data);
+				}else {
+					this.$message.error(res.msg);
+				}
+			},
+			async getWorkingLife(){//工作年限统计
+				let res = await getWorkingLife({
+					pindex: this.pindex,
+					lev   : this.lev
+				});
+				if(res.code === 200) {
+					const data                  = res.data;
+					const { leftBox, rightBox } = this.$refs;
+					data.maxValue = Math.max(...Object.values(data));
+					this.workingLife          = data;
+				}else {
+					this.$message.error(res.msg);
+				}
+			},
+			async getPersonnelStatusQuoList(){//检察机关人员现状
+				let res = await getPersonnelStatusQuoList({
+					pindex: this.pindex,
+					lev   : this.lev
+				});
+				if(res.code === 200) {
+					const data                  = res.data;
+					const { leftBox, rightBox } = this.$refs;
+					this.personnelStatusQuos = data;
+					leftBox.loadpersonnelStatusQuosChart(data);
+				}else {
+					this.$message.error(res.msg);
+				}
+			},
+			async getPersonnelEducation(){//人员学历结构
+				let res = await getPersonnelEducation({
+					pindex: this.pindex,
+					lev   : this.lev
+				});
+				if(res.code === 200) {
+					const data                  = res.data;
+					const { leftBox, rightBox } = this.$refs;
+					this.personnelEducation = data;
+					rightBox.loadEduBgStrChart(data);
+				}else {
+					this.$message.error(res.msg);
+				}
+			},
+			async getAgeDistribution(){//年龄分布
+				let res = await getAgeDistribution({
+					pindex: this.pindex,
+					lev   : this.lev
+				});
+				if(res.code === 200) {
+					const data                  = res.data;
+					const { leftBox, rightBox } = this.$refs;
+					this.ageDistribution = data;
+					rightBox.loadAgeDistributeChart(data);
+				}else {
 					this.$message.error(res.msg);
 				}
 			},
             changeMapState({ pindex, lev }){
 				this.pindex = pindex;
 				this.lev = lev;
-				this.requestTeamData();
-            },
+				// this.requestTeamData();
+				this.getIncumbency();
+				this.getEducationSituation();
+				this.getWorkingLife();
+				this.getPersonnelStatusQuoList();
+				this.getPersonnelEducation();
+				this.getAgeDistribution();            
+			},
 		},
 		components: {
 			Left,
--- src/pages/team-management/service/index.js
+++ src/pages/team-management/service/index.js
@@ -14,7 +14,7 @@ export const getPersonInfo = async (data) => await request({
 	params: data
 });
 
-// 通过姓名查询人员信息
+//通过姓名查询人员信息
 export const getPersonnelPerformance = async (data) => await request({
 	method: 'get',
 	url   : '/api/v1.0/teamManage/getPersonnelPerformance',
@@ -32,5 +32,48 @@ export const getTeamManagement = async (data) => await request({
 export const getTeamManageMap = async (data) => await request({
 	method: 'get',
 	url   : '/api/v1.0/teamManage/getTeamManageMaps',
+	params :data
+});
+
+// 队伍建设:全国在职人数
+export const getIncumbency = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getIncumbency',
+	params :data
+});
+//队伍建设:工作年限统计
+export const getWorkingLife = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getWorkingLife',
+	params :data
+});
+//队伍建设:教育情况
+export const getEducationSituation = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getEducationSituation',
+	params :data
+});
+//队伍建设:各级检察机关人员现状
+export const getPersonnelStatusQuoList = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getPersonnelStatusQuoList',
+	params :data
+});
+//队伍建设:人员学历结构
+export const getPersonnelEducation = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getPersonnelEducation',
+	params :data
+});
+//队伍建设:年龄分布
+export const getAgeDistribution = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getAgeDistribution',
+	params :data
+});
+//队伍建设:最上方-员额内检察官、检察官助理、司法行政人员总数
+export const getPersonnelCategory = async (data) => await request({
+	method: 'get',
+	url   : '/api/v1.0/teamManage/getPersonnelCategory',
 	params :data
 });
