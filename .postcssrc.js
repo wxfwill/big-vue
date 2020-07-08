2b4f643fe8a904d59576c467b700c2412774f557
尉新飞 <18810563686@163.com>
2020-07-08 11:16:02 +0800

    1

--- .postcssrc.js
+++ .postcssrc.js
@@ -19,7 +19,7 @@ module.exports = {
 			utf8: false
 		},
 		"postcss-cssnext"          : {},
-		/*"postcss-px-to-viewport"   : {
+		/*("postcss-px-to-viewport"   : {
 			viewportWidth    : 3840,  //3840,//1255,  // 视窗的宽度，对应的是我们设计稿的宽度，一般是750如果我们设置的宽度是300px，那么编译之后的宽度为(300/750*100)=40vw,如果频宽实际为375px，那么该元素的宽度为（375*0.4）= 150px）
 			viewportHeight   : 1080,//1080,//1000 // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
 			unitPrecision    : 3,     // 指定`px`转换为视窗单位值的小数位数
